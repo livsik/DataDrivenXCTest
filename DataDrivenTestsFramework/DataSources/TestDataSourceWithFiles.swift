0e1a64f02d474bf9eb4cb2257409598f236ce13a
Vasyl Tkachuk <vtkachuk@readdle.com>
2020-11-26 17:43:59 +0200

    Complete implementationof class TestDataSourceWithFiles.

--- DataDrivenTestsFramework/DataSources/TestDataSourceWithFiles.swift
+++ DataDrivenTestsFramework/DataSources/TestDataSourceWithFiles.swift
@@ -6,10 +6,27 @@
 //  Copyright © 2018 Dmitry Protserov. All rights reserved.
 //
 
-class TestDataSourceWithFiles: TestDataSource {
+public class TestDataWithFilePath : TestData {
+    
+    public let filePath: String
+    
+    init(filePath: String) {
+        self.filePath = filePath
+        super.init()
+    }
+}
 
-    init(filesList: [String]) {
+public class TestDataSourceWithFiles: TestDataSource {
+
+    let filesList: [TestDataWithFilePath]
+    
+    public init(filesList: [String]) {
+        self.filesList = filesList.map({ TestDataWithFilePath(filePath: $0) })
         super.init()
     }
     
+    public override func dataCases() -> [TestData] {
+        return self.filesList
+    }
+
 }
